bc6065b90c7bf68159deb681d49a0913af0abb30
ClaudiaDanciu <contact@claudiadanciu.com>
2022-10-12 22:15:21 +0100

    InputName

---
+++ RazorTheFirst/Pages/InputName.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorTheFirst.Pages
+{
+    public class InputNameModel : PageModel
+    { 
+        public string name;
+        public void OnPost()
+            {
+                this.name = Convert.ToString(Request.Form["amount"]);
+            }
+    }
+}
+    
+
