882f8020cc9b05ef518c42adf1d9dcafbbc045e7
ClaudiaDanciu <contact@claudiadanciu.com>
2022-10-12 20:03:46 +0100

    Initial commit

---
+++ RazorTheFirst/Pages/CurrencyConvert.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorTheFirst.Pages
+{
+    public class CurrencyConvertModel : PageModel
+    {
+        public double amount;
+        public const double rate = 1.14;
+        public double currency;
+        public void OnPost()
+        {
+            this.amount = Convert.ToDouble(Request.Form["amount"]);
+            this.currency = amount * rate;
+        }
+    }
+}
